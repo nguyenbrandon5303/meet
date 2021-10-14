3b59acf067a67595fc0104d86dd80bebe12890cc
Brandon Nguyen <51946258+nguyenbrandon5303@users.noreply.github.com>
2021-10-13 20:13:27 -0700

    added offline browsing with PWA

--- src/api.js
+++ src/api.js
@@ -49,9 +49,9 @@ export const getEvents = async () => {
   }
 
   if (!navigator.onLine) {
-    const data = localStorage.getItem("lastEvents");
+    const events = localStorage.getItem("lastEvents");
     NProgress.done();
-    return data ? JSON.parse(events).events : [];;
+    return events ? JSON.parse(events).events : [];;
   }
 
   const token = await getAccessToken();
